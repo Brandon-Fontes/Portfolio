2cf2d790aa36ac60d44fd0ce5215e51786425817
DaFonz <brandonjfontes@gmail.com>
2021-04-13 16:46:09 -0600

    added sidebar

--- portfolio/src/Sidebar.jsx
+++ portfolio/src/Sidebar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Sidebar() {
     return (
         <>
-        <img src="https://images.unsplash.com/photo-1594145070100-c771df5152fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZXJuJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                <img className="fix" src="https://images.unsplash.com/photo-1594145070100-c771df5152fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZXJuJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="gradient" />
+                </div>
+            </div>
+        </div>
         </>
+        
     )
 }
