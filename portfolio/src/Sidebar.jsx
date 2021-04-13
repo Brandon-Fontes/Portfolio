e9813a92d3038b7a08f6eaa27a5b8b8814dfdf6d
DaFonz <brandonjfontes@gmail.com>
2021-04-13 16:31:22 -0600

    removed some files added components and image

---
+++ portfolio/src/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sidebar() {
+    return (
+        <>
+        <img src="https://images.unsplash.com/photo-1594145070100-c771df5152fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZXJuJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        </>
+    )
+}
